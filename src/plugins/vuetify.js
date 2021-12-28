af2fc698e981f1ec5898a23b32b059818e6a26bf
Gokhankr <gokhank58@gmail.com>
2021-12-28 21:58:58 +0300

    Project Bases

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,15 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#3ad08f",
+        secondary: "#695de9",
+      },
+    },
+  },
 });
