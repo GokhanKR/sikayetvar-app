db13fc34c02ce7bc7a645d05766c6ca789f71dc7
Gokhankr <gokhank58@gmail.com>
2021-12-28 21:57:54 +0300

    Added Notify

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-spacer></v-spacer>
-    </v-app-bar>
+  <v-app id="app">
     <v-main>
       <v-container>
         <v-row>
-          <v-col cols="8" class="mx-auto">
-            <text-input v-model="editor" />
+          <v-col cols="12" lg="8" class="mx-auto py-6">
+            <img src="logo.svg" width="200" alt="Logo" />
+          </v-col>
+          <v-col cols="12" lg="8" class="mx-auto py-4">
+            <home />
           </v-col>
         </v-row>
       </v-container>
     </v-main>
+    <!-- Snackbar  -->
+    <v-snackbar
+      v-model="snackbar.value"
+      :timeout="snackbar.timeout"
+      :color="snackbar.status ? 'success' : 'error'"
+    >
+      {{ snackbar.message }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="white"
+          text
+          v-bind="attrs"
+          @click="snackbar.value = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <!-- ./Snackbar  -->
   </v-app>
 </template>
 
 <script>
-import TextInput from "./components/TextInput.vue";
+import Home from "./views/Home.vue";
 
 export default {
   name: "App",
   components: {
-    TextInput,
+    Home,
   },
   data: () => ({
-    editor: ""
+    editor: "",
   }),
+  computed: {
+    /**
+     * @returns {Object}
+     */
+    snackbar() {
+      return this.$store.state.notify;
+    },
+  },
 };
 </script>
+
+<style>
+#app {
+  background-color: #ededed;
+}
+</style>
